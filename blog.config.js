592846ff847fb988f74ff1963f4f5bd5675954b2
三秋十李Sergio <100108379+sanqiushili@users.noreply.github.com>
2023-06-10 01:29:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -43,8 +43,8 @@ const BLOG = {
   notionDomain: 'sqsl.notion.site',
   telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
   telegramChatId: '5540342779', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
+  telegramChannelUrl: 'https://t.me/+RMsO3zyBKkllYTFl', // The link of your Telegram channel
+  telegramChannelName: '三秋的薯条发放站🍟', // The name of your Telegram channel
   craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
   analytics: {
     provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
